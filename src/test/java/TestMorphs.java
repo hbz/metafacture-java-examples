a018bb63f2b9c36b05ae13cc83b37c0baf7351a4
Fabian Steeg <fsteeg@gmail.com>
2017-05-31 11:49:14 +0200

    Add metamorph test framework usage for transformation samples
    Resolves https://github.com/hbz/metafacture-java-examples/issues/1
    
    See also https://github.com/culturegraph/metafacture-ide/issues/42

---
+++ src/test/java/TestMorphs.java
@@ -0,0 +1,16 @@
+import org.culturegraph.mf.test.MetamorphTestSuite;
+import org.culturegraph.mf.test.MetamorphTestSuite.TestDefinitions;
+import org.junit.runner.RunWith;
+
+@RunWith(MetamorphTestSuite.class)
+@TestDefinitions({ //
+		"sample2/mystery-morph-1-test.xml", //
+		"sample2/mystery-morph-2-test.xml", //
+		"sample3/simple-transformation-test.xml", //
+		"sample4/count-test.xml", //
+		"sample5/count-test.xml", //
+		"sample6/enrich-wiki-test.xml", //
+		"sample7/morph-bib-test.xml" })
+public final class TestMorphs {
+	/* bind to xml test */
+}
